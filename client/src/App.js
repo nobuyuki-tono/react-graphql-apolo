8bd6ac9c357ecf33272892a51250bef8f876839d
Nobu <rockmusic2090@gmail.com>
2020-03-12 11:07:11 -0700

    Fetch data from backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import "./App.css";
+import Launches from "./components/Launches";
 
 import logo from "./logo.png";
 
 const client = new ApolloClient({
-  url: "http://localhost:5000/graphql"
+  uri: "http://localhost:5000/graphql"
 });
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           alt="SpaceX"
           style={{ width: 300, display: "block", margin: "auto" }}
         />
+        <Launches />
       </div>
     </ApolloProvider>
   );
--- client/src/components/Launches.jsx
+++ client/src/components/Launches.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import gql from "graphql-tag";
+import React from "react";
+import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
 const LAUNCHES_QUERY = gql`
-  query LaunchesQuery {
+  {
     launches {
       flight_number
       mission_name
@@ -13,14 +13,20 @@ const LAUNCHES_QUERY = gql`
   }
 `;
 
-class Launches extends Component {
-  render() {
-    return (
-      <div>
-        <h1 className="display-4 my-3">Launches</h1>
-      </div>
-    );
-  }
-}
+const Launches = () => {
+  const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
+  return (
+    <>
+      <h1 className="display-4 my-3">Launches</h1>
+      {data.launches.map(launch => {
+        return <LaunchItem key={launch.flight_number} launch={launch} />;
+      })}
+    </>
+  );
+};
 
 export default Launches;
